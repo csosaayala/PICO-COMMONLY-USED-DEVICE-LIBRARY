2a4edf4f16bcdb0e440e5385e9eabbd5be5a0c76
csosaayala <33404881+csosaayala@users.noreply.github.com>
2022-12-13 12:09:21 -0400

    Added the variable inc for increment
    The variable inc was added to vary the increment of the counter.

--- Encoder_C.py
+++ Encoder_C.py
@@ -14,7 +14,7 @@ import machine
 
 class R_Encoder:
    
-    def __init__(self, A_Pin, B_Pin):
+    def __init__(self, A_Pin, B_Pin,inc):
         self.A_Pin = A_Pin
         self.B_Pin = B_Pin
         self.Enc_Counter = 0
@@ -23,7 +23,7 @@ class R_Encoder:
         self.Enc_B_State = 0
         self.Enc_B_State_old = 0
         self.error = 0
-
+        self.inc=inc
         self.Setup_Irqs()    
            
     def DisplayPins(self):
@@ -43,13 +43,13 @@ class R_Encoder:
             # A   B-old
             # 1 & 0 = CW rotation
             # 0 & 1 = CW rotation
-            self.Enc_Counter += 1  #Increment counter by 1 - counts ALL transitions
+            self.Enc_Counter += self.inc  #Increment counter by 1 - counts ALL transitions
         elif (self.Enc_A_State == 1 and self.Enc_B_State_old == 1) or (self.Enc_A_State == 0 and self.Enc_B_State_old == 0):
             # this will be counter-clockwise rotation
             # A   B-old
             # 1 & 1 = CCW rotation
             # 0 & 0 = CCW rotation
-            self.Enc_Counter -= 1 # Decrement counter by 1 - counts ALL transitions
+            self.Enc_Counter -= self.inc # Decrement counter by 1 - counts ALL transitions
         else:  #if here, there is a combination we don't care about, ignore it, but track it for debugging
             self.error += 1
         self.Enc_A_State_old = self.Enc_A_State     # store the current encoder values as old values to be used as comparison in the next loop
@@ -67,3 +67,33 @@ class R_Encoder:
         self.Enc_A_State_old = self.Enc_Pin_A.value()
         self.Enc_B_State_old = self.Enc_Pin_B.value()
 # #--------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------
+# TEST CODE 
+
+# last_Enc_Counter = 0
+# Enc_Counter = 0
+# Last_Qtr_Cntr = 0
+# Qtr_Cntr = 0
+# error = 0
+# import time
+# 
+# 
+# # Main program loop which runs continuously
+# Enc_1 = R_Encoder(17,18,.01)
+# Enc_1.DisplayPins()
+# 
+# while True:
+#     time.sleep(.01)                # Sleep for a moment to slow things down.
+#     Qtr_Cntr = round(Enc_1.Enc_Counter/4,2)   
+# 
+# #     if Enc_1.Enc_Counter != last_Enc_Counter:       
+# #         print(Enc_1.Enc_Counter,Qtr_Cntr)
+# #         last_Enc_Counter = Enc_1.Enc_Counter
+# #         Last_Qtr_Cntr = Qtr_Cntr
+#       
+#     if Qtr_Cntr != Last_Qtr_Cntr:
+#         print(Qtr_Cntr)
+#         last_Enc_Counter = Enc_1.Enc_Counter
+#         Last_Qtr_Cntr = Qtr_Cntr
+#        
+ 
